9e8041f91ae585e9cfffde83d90a37cf9c89f6cd
turtleneckdaddy <la2002ert@gmail.com>
2022-05-17 12:18:51 +0200

    sbona gjo

---
+++ prov/Shared/provApp.swift
@@ -0,0 +1,17 @@
+//
+//  provApp.swift
+//  Shared
+//
+//  Created by Laert Huti on 17.5.22.
+//
+
+import SwiftUI
+
+@main
+struct provApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
