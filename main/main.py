ae7d16a8fa6e9d74022b38c0c765b08aac8f7707
turtleneckdaddy <la2002ert@gmail.com>
2022-05-19 15:42:53 +0200

    did floor, duhet nji app per pixel

--- main/main.py
+++ main/main.py
@@ -5,8 +5,18 @@ from pygame.locals import *
 if __name__== "__main__": #main routine tha ky indiani po se di pse duhet
     pygame.init()
     surface = pygame.display.set_mode((540,720))
-    surface.fill((255, 225, 148))
+    surface.fill((255, 255, 255))
+
+    floor = pygame.image.load("images/floor.png").convert()
+    floor = pygame.transform.scale(floor,(128, 128))
+    surface.blit(floor,(0,592))
+    surface.blit(floor,(128,592))
+    surface.blit(floor,(256,592))
+    surface.blit(floor,(384,592))
+    surface.blit(floor,(512,592))
     pygame.display.flip()
+
+
     running = True
 
     while running:
